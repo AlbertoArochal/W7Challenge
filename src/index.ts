617a4be7078fda246d20d0758c97f99776bfeca2
AlbertoArochal <arochaldev@gmail.com>
2022-11-24 07:56:01 +0100

    talk button half backed

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,6 @@ import {
     theGang,
     characterSheet,
     fillChSheet,
-    buttonTalk,
 } from "./app.js";
 
-/*document.addEventListener("DOMContentLoaded", () => {
-    const slots = document.querySelectorAll("slot");
-    slots.forEach((item, i) => (item.outerHTML = characterSheet(theGang[i])));
-});*/
-
 theGang.forEach((char) => fillChSheet(char));
-buttonTalk();
